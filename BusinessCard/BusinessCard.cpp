0450f78d5bfd8bd9ba8e1d5fa6b4152c6946f6a3
jgamez118 <47163834+jgamez118@users.noreply.github.com>
2019-02-07 17:49:32 -0600

    Add files via upload

---
+++ BusinessCard/BusinessCard.cpp
@@ -0,0 +1,29 @@
+// BusinessCard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "JAG Business Card" << endl;
+	cout << "=================" << endl;
+	cout << "1010 King Court" << endl;
+	cout << "JohnG@gmail.com" << endl;
+	cout << "Personal (210)-073-1985" << endl;
+	cout << "Fax (210)-214-2016" << endl;
+	cout << "Facebook.com/JohnGomez" << endl;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
