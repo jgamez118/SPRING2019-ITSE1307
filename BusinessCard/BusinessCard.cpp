f864b82a6147e4fa9e3c32b45b37873899f05407
jgamez118 <47163834+jgamez118@users.noreply.github.com>
2019-02-07 17:52:09 -0600

    Update BusinessCard.cpp

--- BusinessCard/BusinessCard.cpp
+++ BusinessCard/BusinessCard.cpp
@@ -8,22 +8,12 @@ using namespace std;
 int main()
 {
     cout << "JAG Business Card" << endl;
-	cout << "=================" << endl;
-	cout << "1010 King Court" << endl;
-	cout << "JohnG@gmail.com" << endl;
-	cout << "Personal (210)-073-1985" << endl;
-	cout << "Fax (210)-214-2016" << endl;
-	cout << "Facebook.com/JohnGomez" << endl;
+    cout << "=================" << endl;
+    cout << "1010 King Court" << endl;
+    cout << "JohnG@gmail.com" << endl;
+    cout << "Personal (210)-073-1985" << endl;
+    cout << "Fax (210)-214-2016" << endl;
+    cout << "Facebook.com/JohnGomez" << endl;
 
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
